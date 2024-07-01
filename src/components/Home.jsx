97984a8d773605ea347edfd5c43ff2883e669461
SIPHO <sipho@siliconmaboneng.co.za>
2024-07-01 19:03:47 +0200

    Small Updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     <div name="home" className="h-screen w-screen bg-gradient-to-b from-black via-black to-gray-800">
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
             <div className="flex flex-col justify-center h-full">
-                <h2 className="text-4xl sm:text-7xl font-bold text-white">I am a Full Stack Developer</h2>
+                <h2 className="text-4xl sm:text-7xl font-bold text-white">I am an Ispiring Full Stack Developer</h2>
                 <p className="text-gray-500 py-4 max-w-md ">
                     I have 2+ years of experience in web development. Currently leading a team of developers, 
                     who are building what we call a hubspot for entrepruneurs using Angular, bootstrap, primeng, C# and SQLServer.
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -46,8 +46,8 @@ const Portfolio = () => {
                 alt=""
                 className="rounded-md duration-200 hover:scale-105"/>
                 <div className='flex items-center justify-center'>
-                        <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105' >Demo</button>
-                  <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'><a  rel="noreferrer" href={codeHref} target='_blank' >Code</a></button>
+                {/* <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105' >Demo</button>
+                  <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'><a  rel="noreferrer" href={codeHref} target='_blank' >Code</a></button> */}
                 </div>
                 </div>  
            
